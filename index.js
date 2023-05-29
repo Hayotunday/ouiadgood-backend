50024c34566667b627208beb1fa999f95f576c3d
Hayotunday <idowudanielayotunde@gmail.com>
2023-05-29 13:43:56 +0100

    Added money route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 import usersRoutes from './routes/users.js'
 import notesRoutes from './routes/notes.js'
+import moneyRoutes from './routes/money.js'
 
 import Money from './model/money.js'
 
@@ -20,6 +21,7 @@ connectToDB();
 
 app.use('/users', usersRoutes);
 app.use('/notes', notesRoutes);
+app.use('/money', moneyRoutes);
 
 const newMoney = new Money({ totalmoney: 0 });
 
@@ -27,13 +29,18 @@ newMoney.save()
   .then(() => { console.log('Money created!') })
   .catch((err) => { console.log('Error: ' + err) })
 
+Money.deleteMany({ totalmoney: 0 }, {})
+  .then(() => { console.log('Money deleted!') })
+  .catch((err) => { console.log('Error: ' + err) })
+
 let value = 0.1;
+var result = {};
 
 if (connectToDB) {
   setInterval(async () => {
     await Money.findOneAndUpdate({}, { $inc: { totalmoney: value } })
-      .then((result) => {
-        console.log('Value increased:', result);
+      .then((res) => {
+        result = { ...result, res }
       })
       .catch((error) => {
         console.error('Failed to increase value:', error);
---
+++ routes/money.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Money from "../model/money.js";
+
+const router = express.Router();
+
+// all routes in here are starting with /money
+
+// Money
+router.get('/', (req, res) => {
+  Money.find()
+    .then((money) => { res.json(money) })
+    .catch((err) => { res.status(400).json('Error: ' + err) })
+});
+
+export default router
